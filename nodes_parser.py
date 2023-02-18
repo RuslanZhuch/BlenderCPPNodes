31f49c9eb0da65f20cbcea0f501545104c6284eb
RuslanZhuch <ruslanacs93@mail.ru>
2023-02-18 18:30:19 +0600

    Parsed nodes json output

--- nodes_parser.py
+++ nodes_parser.py
@@ -14,6 +14,12 @@ def traverse_node_inputs(node, nodes_to_traverse_next, nodes_cache):
     inputs = node.inputs
     print("Node {} has inputs {}".format(node, inputs))
 
+    node_data = {
+        "id": node.bl_idname,
+        "name": node.name,
+        "inputs": []
+    }
+
     for node_input in inputs:
         print("Process inputs:", node_input)
         print("Is linked", node_input.is_linked)
@@ -26,7 +32,28 @@ def traverse_node_inputs(node, nodes_to_traverse_next, nodes_cache):
         from_node = link.from_node
         print("From node", from_node)        
         from_output = link.from_socket
-        print("From щгезге", from_output)
+        print("From socket", from_output)
+
+        def find_output_socket_id(node_to_find_in, socket_to_find):
+            node_outputs = node_to_find_in.outputs
+            #TODO: Remake it in python way 
+            index = 0
+            for output in node_outputs:
+                if output == socket_to_find:
+                    return index
+                index += 1
+
+            return -1
+
+        from_output_index = find_output_socket_id(from_node, from_output)
+
+        print("From socket if", from_output_index)
+
+        node_data["inputs"].append({
+            "target_node_id": from_node.bl_idname,
+            "target_node_name": from_node.name,
+            "target_socket_id": from_output_index
+        })
 
         already_in_list = from_node in nodes_cache
         print("Is Node already listed", already_in_list)
@@ -35,6 +62,8 @@ def traverse_node_inputs(node, nodes_to_traverse_next, nodes_cache):
             nodes_cache.add(from_node)
             nodes_to_traverse_next.put(from_node)
             print("Added node {} to list".format(from_node))
+
+    return node_data
     
 def traverse_nodes(node_group):
     nodes = node_group.nodes
@@ -48,8 +77,17 @@ def traverse_nodes(node_group):
     nodes_to_traverse_next.put_nowait(output_node)
     nodes_cache = set()
 
+    json_data = {
+        "version": 1,
+        "data":[]
+    }
+
     while not nodes_to_traverse_next.empty():
         node_to_traverse = nodes_to_traverse_next.get_nowait()
         print("Node to traverse:", node_to_traverse)
 
-        traverse_node_inputs(node_to_traverse, nodes_to_traverse_next, nodes_cache)
+        json_data["data"].append(traverse_node_inputs(node_to_traverse, nodes_to_traverse_next, nodes_cache))
+
+        output_path = bpy.context.scene.cppgen.src_path + "temp\\parsed_nodes.json"
+        with open(output_path, 'w', encoding='utf-8') as f:
+            json.dump(json_data, f, ensure_ascii=False, indent=4)     
