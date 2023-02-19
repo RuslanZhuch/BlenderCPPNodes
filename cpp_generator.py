631fe1ae866909e7c0ff14697cdb18012e39768a
RuslanZhuch <ruslanacs93@mail.ru>
2023-02-20 02:27:26 +0600

    basic args generation

--- cpp_generator.py
+++ cpp_generator.py
@@ -1,14 +1,19 @@
 import json
 
+
+def gather_node_data(schema, node_name):
+    data = schema["data"]
+    for node in data:
+        if node["name"] == node_name:
+            return node
+
+    return None
+
 def generate_signature(schema):
     def generate_arguments(schema):
         args = ""
         data = schema["data"]
-        node_data = None
-        for node in data:
-            if node["name"] == "Input":
-                node_data = node
-                break
+        node_data = gather_node_data(schema, "Input")
         
         outputs_schema = node_data["outputsSchema"]
         outputs_names = outputs_schema["names"]
@@ -23,3 +28,27 @@ def generate_signature(schema):
 
     args = generate_arguments(schema)
     return "auto code({})".format(args)
+
+def generate_call_args(function_schema):
+    inputs = function_schema["inputs"]
+    args = []
+    for input_data in inputs:
+        if input_data["target_node_name"] == "Input":
+            args.append("arg{}".format(input_data["target_socket_id"] + 1))
+    
+    args_string = ""
+    for arg in args:
+        if arg != args[0]:
+            args_string += ", "
+        args_string += arg
+
+    return args_string
+
+def generate_function_call(function_schema):
+    return "const decltype(auto) {}Result{{ {} }};".format(
+        function_schema["name"],
+        "{}({})".format(
+            function_schema["name"],
+            generate_call_args(function_schema)
+        )
+    )
--- tests_cppgenerator.py
+++ tests_cppgenerator.py
@@ -12,7 +12,28 @@ sys.path.append(root_dir)
 
 import cpp_generator
 
+def gather_node_data_input(schema_data):
+    data = schema_data["data"]
+    node_data = None
+    for node in data:
+        if node["name"] == "Input":
+            return node    
+
+def gather_node_data_output(schema_data):
+    data = schema_data["data"]
+    node_data = None
+    for node in data:
+        if node["name"] == "Output":
+            return node 
+        
 class TestCppGenerator(unittest.TestCase):
+
+    def test_gather_node_data(self):
+        schema_file = open("tests\\resources\\nodes_1_schema.json")
+        schema = json.load(schema_file)
+        self.assertEqual(cpp_generator.gather_node_data(schema, "Input"), gather_node_data_input(schema))
+        self.assertEqual(cpp_generator.gather_node_data(schema, "Output"), gather_node_data_output(schema))
+        schema_file.close()
 
     def test_gen_high_level_signature_1(self):
         schema_file = open("tests\\resources\\nodes_1_schema.json")
@@ -26,6 +47,22 @@ class TestCppGenerator(unittest.TestCase):
         schema = json.load(schema_file)
         signature = cpp_generator.generate_signature(schema)
         self.assertEqual(signature, "auto code(auto&& arg1, auto&& arg2)")
+        schema_file.close()
+
+    def test_gen_args_for_schema2_and(self):
+        schema_file = open("tests\\resources\\nodes_2_schema.json")
+        schema = json.load(schema_file)
+        function_schema = cpp_generator.gather_node_data(schema, "and")
+        generated = cpp_generator.generate_call_args(function_schema)
+        self.assertEqual(generated, "arg1, arg2")
+        schema_file.close()
+
+    def test_gen_schema1_not_function_call(self):
+        schema_file = open("tests\\resources\\nodes_1_schema.json")
+        schema = json.load(schema_file)
+        function_schema = cpp_generator.gather_node_data(schema, "not")
+        generated = cpp_generator.generate_function_call(function_schema)
+        self.assertEqual(generated, "const decltype(auto) notResult{ not(arg1) };")
         schema_file.close()
 
 if __name__ == '__main__':
